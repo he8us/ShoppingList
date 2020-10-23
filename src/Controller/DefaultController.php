94a3ee769d98778572e9d47249bbf36320b2daf9
Cédric Michaux <cedric@he8us.be>
2020-10-23 23:23:47 +0200

    Check db connection from web

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="default")
+     */
+    public function index(): Response
+    {
+        $repo = $this->getDoctrine()->getRepository(Product::class);
+        return $this->render('default/index.html.twig', [
+            'items' => $repo->findAll(),
+        ]);
+    }
+}
