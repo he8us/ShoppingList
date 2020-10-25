accf8b771b94d323a9d01ca4a3de3885570159d4
Cédric Michaux <cedric@he8us.be>
2020-10-25 20:39:18 +0100

    UserResolver  tests

--- tests/Unit/GraphQL/Resolver/UserResolverTest.php
+++ tests/Unit/GraphQL/Resolver/UserResolverTest.php
@@ -4,6 +4,52 @@
 namespace App\Tests\GraphQL\Resolver;
 
 
-class UserResolver {
+use App\Entity\User;
+use App\GraphQL\Resolver\UserResolver;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManager;
+use InvalidArgumentException;
+use Overblog\GraphQLBundle\Definition\Argument;
+use PHPUnit\Framework\TestCase;
+
+class UserResolverTest extends TestCase {
+
+    public function testResolve_withArgument(): void {
+        $user           = new User();
+        $repositoryMock = $this->createMock(UserRepository::class);
+
+        $repositoryMock->expects($this->once())
+                       ->method("find")
+                       ->with(10)
+                       ->willReturn($user);
+
+
+        $entityManagerMock = $this->createMock(EntityManager::class);
+
+        $entityManagerMock->method("getRepository")
+                          ->with(User::class)
+                          ->willReturn($repositoryMock);
+
+        $resolver = new UserResolver($entityManagerMock);
+
+        $this->assertEquals($user, $resolver->resolve(new Argument(['id' => 10])));
+    }
+
+    public function testResolve_withoutArgument(): void {
+        $entityManagerMock = $this->createMock(EntityManager::class);
+        $resolver          = new UserResolver($entityManagerMock);
+
+        $this->expectException(InvalidArgumentException::class);
+        $resolver->resolve(new Argument());
+
+    }
+
+    public function testGetAliases(): void {
+        $excpected = [
+            'resolve' => 'User',
+        ];
+
+        $this->assertEquals($excpected, UserResolver::getAliases());
+    }
 
 }
